98c1bfcef00354d0165172472cd7392dff6d61f9
lenaMarilena <112854347+lenaMarilena@users.noreply.github.com>
2022-11-10 14:30:30 +0100

    examples of words

---
+++ src/Example.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Example.css';
+
+export default function Example(props) {
+  if (props.example) {
+    return (
+      <div className="Example">
+        <p>{props.example}</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
